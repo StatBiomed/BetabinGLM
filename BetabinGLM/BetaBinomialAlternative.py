3e13665e26e4e8182074876007cf103f86a0857c
Jinhui Liu <96280063+Liu-Jinhui@users.noreply.github.com>
2022-11-02 18:20:09 +0800

    Add descriptions

--- BetabinGLM/BetaBinomialAlternative.py
+++ BetabinGLM/BetaBinomialAlternative.py
@@ -4,9 +4,11 @@ import statsmodels.api as sm
 from scipy.optimize import minimize
 from scipy.special import betaln, gammaln
 
-
-# sigmoid function
 def sigmoid(x):
+    """sigmoid function
+
+    Returns: np.array(a) containing all the sigmoid values of input array x
+    """
     a = []
     for i in np.nditer(x):
         if i >= 0:
@@ -15,8 +17,23 @@ def sigmoid(x):
             a.append(math.exp(i)/(1+math.exp(i)))
     return np.array(a)
 
-# cost function used for optimizing the weights
 def cost_fun_for_W(W, endog, exog, phi):
+    """cost function used for optimizing the weights
+
+        Z (np.array): sigmoid values of current prediction
+        k (np.array): the number of positive events
+        n (np.array): the number of total events
+        phi (double): overdispersion value, equals to 1/(1 + alpha + beta)
+        a (double): alpha
+        b (double): beta
+
+    Returns:
+        np.array: value of cost function under current parameter values
+
+    Note: 
+        Some constants occured in the cost function have been ignored here as it won't affect the function trend. 
+        This function is same as the function cost_fun_for_W. Seperating them into two is just for preventing the scipy.optimize.minimize to mess up the parameter of optimization.
+    """
     Z = sigmoid(np.inner(W, exog))
     k = np.array(endog[:, 0])
     n = np.array(endog[:, 0] + endog[:, 1])
@@ -25,8 +42,23 @@ def cost_fun_for_W(W, endog, exog, phi):
     cost = np.sum(betaln(k + a, n - k + b) - betaln(a, b))
     return -(cost/np.size(Z))
 
-# cost function used for optimizing phi
 def cost_fun_for_phi(phi, endog, exog, W):
+    """cost function used for optimizing phi
+
+        Z (np.array): sigmoid values of current prediction
+        k (np.array): the number of positive events
+        n (np.array): the number of total events
+        phi (double): overdispersion value, equals to 1/(1 + alpha + beta)
+        a (double): alpha
+        b (double): beta
+
+    Returns:
+        np.array: value of cost function under current parameter values
+
+    Note: 
+        Some constants occured in the cost function have been ignored here as it won't affect the function trend. 
+        This function is same as the function cost_fun_for_phi. Seperating them into two is just for preventing the scipy.optimize.minimize to mess up the parameter of optimization.
+    """
     Z = sigmoid(np.inner(W, exog))
     k = np.array(endog[:, 0])
     n = np.array(endog[:, 0] + endog[:, 1])
@@ -35,8 +67,17 @@ def cost_fun_for_phi(phi, endog, exog, W):
     cost = np.sum(betaln(k + a, n - k + b) - betaln(a, b))
     return -(cost/np.size(Z))
 
-# predict the output given the parameters
 def predict(self):
+    """predict the output given the parameters
+
+        Z (np.array): sigmoid values of current prediction
+        n (np.array): the number of total events
+        pred (list): a (2 * number of cases) list of predicted beta-binomial distribution with current optimized weight
+        ypred (np.array): ypred in the type of np.array
+
+    Returns:
+        ypred(np.array): predicted beta-binomial distributions
+    """
     W = self.W
     Z = sigmoid(np.inner(W, self.exog))
     n = np.array(self.endog[:, 0] + self.endog[:, 1])
@@ -44,8 +85,19 @@ def predict(self):
     ypred = np.array(pred)
     return ypred
 
-# calculate the log likelihood after optimization
 def get_loglikelihood(self):
+    """calculate the log likelihood after optimization
+
+        Z (np.array): sigmoid values of current prediction
+        k (np.array): the number of positive events
+        n (np.array): the number of total events
+        phi (double): overdispersion value, equals to 1/(1 + alpha + beta)
+        a (double): alpha
+        b (double): beta
+
+        Returns: 
+            the log-likelihood value
+    """
     W = self.W
     phi = self.phi
     Z = sigmoid(np.dot(self.exog, W))
@@ -55,8 +107,23 @@ def get_loglikelihood(self):
     b = (1 - Z) * (1/phi - 1)
     return np.sum(betaln(k + a, n - k + b) - betaln(a, b)) + np.sum(gammaln(n + 1) - gammaln(k + 1) - gammaln(n - k + 1))
 
-# optimize the parameters once
 def fit_trial(self, phi, W):
+    """optimize the parameter once
+
+    First optimize the weight (fix the value of phi), then optimize phi (fix the weight to the optimized one)
+
+        bounds_W (list): the bounds set for optimizing W using scipy.optimize.minimize
+        res1: OptimizeResult of W
+        W (np.array): optimized result
+        bounds_W (list): the bound set for optimizing phi using scipy.optimize.minimize
+        res2: OptimizeResult of phi
+    
+    Returns:
+        np.array, np.array: the optimized result of the weight and the phi
+
+    Note: 
+        for more about the OptimizeResult using scipy.optimize.minimize, please refer to https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
+    """
     bounds_W = [(-np.inf, np.inf) for i in range (self.exog.shape[1])] 
     res1 = minimize(fun = cost_fun_for_W, x0 = W, args = (self.endog, self.exog, phi), bounds = bounds_W, method = self.method, options = {'maxiter': 50 * self.exog.shape[1]}) 
     W = res1.x
@@ -64,8 +131,22 @@ def fit_trial(self, phi, W):
     res2 = minimize(fun = cost_fun_for_phi, x0 = phi, args = (self.endog, self.exog, W), bounds = bounds_phi, method = self.method, options = {'maxiter': 10}) 
     return res1.x, res2.x
 
-#  calculate the log likelihood during optimization
 def get_LL(self, W, phi):
+    """calculate the log likelihood during optimization
+
+        Z (np.array): sigmoid values of current prediction
+        k (np.array): the number of positive events
+        n (np.array): the number of total events
+        phi (double): overdispersion value, equals to 1/(1 + alpha + beta)
+        a (double): alpha
+        b (double): beta
+
+    Returns: 
+        double: the log-likelihood value
+
+    Note:
+        The calculation is same as the function get_loglikelihood. But, during the optimization process, the intermediate values of W and phi will not be assigned to self. Therefore, this additional function is necessary as it contains additional args W and phi.
+    """
     Z = sigmoid(np.dot(self.exog, W))
     k = np.array(self.endog[:, 0])
     n = np.array(self.endog[:, 0] + self.endog[:, 1])
@@ -73,11 +154,19 @@ def get_LL(self, W, phi):
     b = (1 - Z) * (1/phi - 1)
     return np.sum(betaln(k + a, n - k + b) - betaln(a, b)) + np.sum(gammaln(n + 1) - gammaln(k + 1) - gammaln(n - k + 1))
 
-# beta-binomial regression
 def fit(self):
+    """ beta-binomial regression
+
+    An expectation-maximization like method for beta-binomial regression. The function will keep optimization until the negative log-likelihood change is smaller than 1e-7
+
+    NLL_trend (list): store the negative log-likelihood after each optimization trial
+
+    Returns:
+        np.array, np.array: the optimized value of W and phi
+
+    """
     phi = 0.1
     W = np.array([0 for i in range (self.exog.shape[1])])
-    #print(np.shape(W))
     NLL_trend = []
     while True:
         if (len(NLL_trend) > 1):
